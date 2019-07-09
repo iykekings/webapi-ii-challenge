4158ce9163d7c2c467673d2ed0a7770af6f2cbf8
Ikechukwu <iykekings36@gmail.com>
2019-07-09 12:09:56 +0100

    Setup eslint

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const postsRouter = require('./routes/posts');
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
+server.get('/', (req, res) => {
+  res.status(200).json('Post server running at /posts');
+});
+
+server.use('/posts', postsRouter);
+
 server.listen(4000, () => {
-  console.log('Srever running at http://localhost:4000');
+  console.log('Server running at http://localhost:4000');
 });
---
+++ routes/posts.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const PostsDB = require('../data/db');
+const postsRouter = express.Router();
+
+module.exports = postsRouter;
