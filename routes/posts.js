939b50cf550897c72166cb726e77b4713c93db24
Ikechukwu <iykekings36@gmail.com>
2019-07-09 12:32:51 +0100

    implement get posts end point

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,19 @@
 const express = require('express');
-// const PostsDB = require('../data/db');
+const PostsDB = require('../data/db');
 
 const postsRouter = express.Router();
 
-postsRouter.get('/', (req, res) => {
-  res.status(200).json('cool');
+postsRouter.get('/', async (req, res) => {
+  try {
+    const posts = await PostsDB.find();
+    if (posts.length) {
+      res.status(200).json(posts);
+    } else {
+      res.status(404).json({ error: 'posts not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ errorMessage: "Couldn't fetch posts" });
+  }
 });
 
 module.exports = postsRouter;
