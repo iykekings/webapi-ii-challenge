40e21de2ccaebc136b958ae184d0e5fc159c819f
Ikechukwu <iykekings36@gmail.com>
2019-07-09 13:27:24 +0100

    implement add post

--- routes/posts.js
+++ routes/posts.js
@@ -18,7 +18,7 @@ postsRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const post = await PostsDB.findById(id);
-    if (post) {
+    if (post.length) {
       res.status(200).json(post);
     } else {
       res.status(404).json({ message: 'The post with the specified ID does not exist.' });
@@ -43,4 +43,33 @@ postsRouter.get('/:id/comments', async (req, res) => {
   }
 });
 
+postsRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const toBeDeleted = await PostsDB.findById(id);
+  if (Object.keys(toBeDeleted).length) {
+    try {
+      await PostsDB.remove(id);
+      res.status(200).json(toBeDeleted);
+    } catch (error) {
+      res.status(500).json({ error: 'The post could not be removed' });
+    }
+  } else {
+    res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+  }
+});
+
+postsRouter.post('/', async (req, res) => {
+  const { title, contents } = req.body;
+  if (title && contents) {
+    try {
+      const newPost = await PostsDB.insert({ title, contents });
+      const addedPost = await PostsDB.findById(newPost.id);
+      res.status(201).json(addedPost[0]);
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error while saving the post to the database' });
+    }
+  } else {
+    res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+  }
+});
 module.exports = postsRouter;
