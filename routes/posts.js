548c50584b8fa26c1f6ab9b53661d24b889b6516
Ikechukwu <iykekings36@gmail.com>
2019-07-09 12:14:12 +0100

    linked postRouter with main app

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const PostsDB = require('../data/db');
+// const PostsDB = require('../data/db');
+
 const postsRouter = express.Router();
+
+postsRouter.get('/', (req, res) => {
+  res.status(200).json('cool');
+});
 
 module.exports = postsRouter;
